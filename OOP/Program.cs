ea83a2c0a6f58653b6f6060597e5c1868d2585ba
Al Momen <almomen535@gmail.com>
2025-03-05 00:20:58 +0600

    Add project files.

---
+++ OOP/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace OOP
+{
+
+   class Person
+    {
+        public string name;
+        public int age;
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.name = "Al Momen";
+            p1.age = 24;
+            Console.WriteLine("Person1:");
+            Console.WriteLine($"Name: {p1.name},Age: {p1.age}");
+
+
+            Person p2 = new Person();
+            p2.name = "Samiul Islam";
+            p2.age = 26;
+            Console.WriteLine("Person2: ");
+            Console.WriteLine($"Name: {p2.name}, Age: {p2.age}");
+        }   
+    }
+}
