06a86a87040da0dbd546f78fb56e63ab1497e07e
Al Momen <almomen535@gmail.com>
2025-03-06 07:26:11 +0600

    Added Contructor's are default and parameterized

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 using System.Runtime.Serialization;
 
 namespace OOP
@@ -11,19 +12,41 @@ namespace OOP
         public int age;
 
 
-        // Method-> DisplayInfo
-        public void DisplayInfo()
-        {
-            Console.WriteLine($"Name: {name},Age: {age}\n");
-        }
 
+        /* 
+         
+         Constructor: A special method which is used for Initialize the object's value and it is exactly the same name of the class.
+         In this case, the class is Person. A normal method works when it is called, but there is no need to call
+         the constructor. The constructor will be called instantly when an object is created.
+        
+         */
 
-        // Method-> SetValue
-        public void SetValue(string n,int a)
+        public Person(string n, int a)  // Parameterized Constructor
         {
             name = n;
             age = a;
         }
+
+        public Person()
+        {
+            name = "Test";
+            age = 15;
+        }
+
+
+        // Method-> DisplayInfo
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {name} and Age: {age}\n");
+        }
+
+
+        /*//Method-> SetValue
+        public void SetValue(string n,int a)
+         {
+            name = n;
+           age = a;
+        }*/
 
 
     }
@@ -32,21 +55,22 @@ namespace OOP
     {
         static void Main(string[] args)
         {   
-            //Object-> p1
-            Person p1 = new Person();
 
-            p1.SetValue("Al Momen", 24);
+            //Object-> p1
+            Person p1 = new Person("Al Momen",24);
             p1.DisplayInfo();
             
          
 
             // Object-> p2
-            Person p2 = new Person();
-            
-            p2.SetValue("Muntasir Asif", 25);
+            Person p2 = new Person("Muntasir Asif",25);
             p2.DisplayInfo();
-            
-            
+
+
+            //Object-> 3
+            Person p3 = new Person();
+            Console.WriteLine("This is default constructor example below:  ");
+            p3.DisplayInfo();            
         }   
     }
 }
